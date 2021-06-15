8aa0034404eeb73f1977cf44d37a083994b452a1
pablo <perez51160900@hotmail.com>
2021-06-15 13:25:47 -0400

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container row">
+    <div class="col-2">
+				<select id='paises' class="browser-default" v-model="selected">
+					<option v-for="(country, index) in countriesFromApi" 
+							:value="country" 
+							:key='index' >{{country.name}} - {{country.currencyId}}</option>
+				</select>
+			</div>
+      <br>
+    <exchange-component :countryName="selected.name"
+    :countryFlag="selected.id"
+    :countrySymbol="selected.alpha3"
+    :currency="selected.currencyName"
+    :exchange="1"></exchange-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import '../node_modules/materialize-css/dist/css/materialize.css';
+import '../node_modules/materialize-css/dist/js/materialize.js';
+import ExchangeComponent from './components/Exchange.vue'; 
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ExchangeComponent,
+  },
+  data:function(){
+    return {
+      countriesFromApi:[],
+      selected:''
+      
+    }
+  },
+  beforeCreate:function(){
+	axios
+	.get('https://free.currconv.com/api/v7/countries?q=USD_PHP&compact=ultra&apiKey=cd551d036c623e3af46d')
+	.then((response) => {
+	Object.entries(response.data.results).sort().forEach(([key, value])=>{
+		console.log(key)
+    console.log(value)
+		this.countriesFromApi.push(value)
+	})
+	})
+  }, 
+  beforeUpdate:function(){
+    console.log(this.selected)
   }
 }
 </script>
@@ -21,7 +55,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Exchange.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="row indigo darken-4" id="contenido">
+        <h4 class="white-text">Convertidor de Monedas</h4>
+        <div class="col s5 white" id="us">
+            <div class="row">
+                <div class="col s2">
+                    <img src="https://www.countryflags.io/us/flat/64.png">
+                </div>
+                <div class="col s6">
+                    <h5>Estados Unidos</h5>
+                </div>
+            </div>
+            <div class="row">
+                <p>Valor de cambio {{usValue}} USD</p>    
+                <div class="input-field col s6">
+                    <input id="us_value" type="number" class="validate" v-model="dolars">
+                    <label class="active" for="us_value">Cantidad en dolares</label>
+                </div>
+            </div>
+        </div> 
+        <div class="col s2"></div> 
+        <div class="col s5 white" id="other">
+            <div class="row">
+                <div class="col s2">
+                    <img v-if='countryFlag' :src='countryFlag'>
+                </div>
+                <div class="col s6">
+                    <h5>{{countryName}}</h5>
+                </div>
+            </div>
+            <div class="row">
+                <p>Valor de cambio {{exchange}} {{countrySymbol}}</p>    
+                <div class="input-field col s7">
+                    <input id="us_value" type="number" class="validate" v-model="dolars">
+                    <label class="active" for="us_value">Cantidad en {{currency}}</label>
+                </div>
+            </div>
+        </div>  
+    </div>
+</template>
+<script>
+    export default {
+        name:"exchange-component", 
+        props:{
+            countryFlag:String, 
+            exchange:Number, 
+            countryName:String,
+            exchangeValue:Number, 
+            countrySymbol:String,
+            currency:String
+        }, 
+        data:function(){
+            return {
+                usValue:1,
+                dolars:0,
+            }
+        },
+        beforeUpdate:function(){
+            console.log(this.countryFlag)
+            if(this.countryFlag){
+                this.countryFlag=`https://www.countryflags.io/${this.countryFlag}/flat/64.png`
+            }       
+        }
+    }
+</script>
+<style>
+    #contenido{
+        padding: 2.5%;
+    }
+    #us{
+        padding: 2.5%;
+    }
+    #other{
+        padding: 2.5%;
+    }
+</style>
